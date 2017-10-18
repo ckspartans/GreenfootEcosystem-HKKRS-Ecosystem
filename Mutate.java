f13bc2e66552214c48938c8a470faec405f63bb4
HectorSalermo <32369070+HectorSalermo@users.noreply.github.com>
2017-10-17 23:45:40 -0400

    Update Mutate.java

--- Mutate.java
+++ Mutate.java
@@ -4,16 +4,13 @@ import java.util.*;
 public class Mutate  
 {
     int mutatedDna;
-    static boolean benefitial = true; //for future if we want to determine whether or not a mutation is benefitial or not
+    //static boolean beneficial = true; //for future if we want to determine whether or not a mutation is benefitial or not//next iteration
     static int[] importedDna; //in array to hold dna value of current organism
 
     /**
      * Constructor for objects of class Mutate
      */
-    public Mutate()
-    {
-        
-    }
+ 
     
     //Different functions for different organism due to different traits associated with each organism
     
@@ -22,30 +19,34 @@ public class Mutate
        importedDna = new int[]{lifespan, reproduce, def}; //Set values of array to the parameters given
 
        for(int j = 0; j < importedDna.length; j++){
-          importedDna[j] -= Greenfoot.getRandomNumber(200);//test number to confirm mutation works and can modify values by given random number
-           
-          /*
-          if(benefitial){
+           if (importedDna[0] < 900){ 
+               importedDna[j] += Greenfoot.getRandomNumber(200)-100;//test number to confirm mutation works and can modify values by given random number
+           }
+           /*
+           if(benefitial){
               importedDna[j] += Greenfoot.getRandomNumber(10) - 5; // substitute int in randomNumber later with a user changable varialbe to determine how good/bad a mutation is
-          } else {
+           } else {
               importedDna[j]-= Greenfoot.getRandomNumber(10); 
-          }
-          */
+           }
+           */
        }
        
        return importedDna; //function sends back new modified algae dna values
     }
     
     
-    public static int[] mutatePlanteater(int speed, int health, int trackingDistance){ //etc, add mroe as needed
-       importedDna = new int[]{speed, health, trackingDistance}; //add more dna traits such as tracking range etc as needed
+    public static int[] mutatePlantEater(int speed, int sight, int repro_energy){ //etc, add mroe as needed
+       importedDna = new int[]{speed, sight, repro_energy}; //add more dna traits such as tracking range etc as needed
         
        for(int j = 0; j < importedDna.length; j++){
-          if(benefitial){
+          if (importedDna[0] < 900){ 
+               importedDna[j] += Math.abs(Greenfoot.getRandomNumber(2)-1);//test number to confirm mutation works and can modify values by given random number
+           }
+           /*if(beneficial){
               importedDna[j] += Greenfoot.getRandomNumber(3);//(int)maxGood);
           } else {
               importedDna[j]-= Greenfoot.getRandomNumber(2);
-          }
+          }*/
        }
         
         
